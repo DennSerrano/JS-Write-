aae09cc46503cf2c0c97f4d2683af4c5c1e3b096
Dennserrano <dennserrano@gmail.com>
2021-10-25 14:43:14 +0200

    JSWrite

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+document.write("Hello World!")
+document.write("<h1>Hello World!</h1>")
+document.write("<p>Hello World!</p>")
+
+let myText = "Hello again"
+
+document.write(myText)
+
+let vorName = "NAME: Dennis "
+let nachName = "Serrano "
+document.write(vorName + nachName)
